0d7be647a6c5ea21238c8ff84e297b4cf3483aab
AnastasiiaTitarenko <151573636+AnastasiiaTitarenko@users.noreply.github.com>
2024-04-15 14:51:13 +0300

    11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -15,11 +15,45 @@ const searchInput = document.querySelector("[search-form-input]");
 
 btnSubmit.disabled = true;
 
-btnSubmit.addEventListener("submit", handleSubmit);
+searchForm.addEventListener("submit", handleSubmit);
+
+
+
+function makeRequest(searchText) {
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: searchText,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+    });
+
+    fetch(`${BASE_URL}?${params}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status)
+            }
+            return response.json()
+        })
+        .then((data) => {
+            if (data.hits.length === 0) {
+                iziToast.error({
+                    title: "Error",
+                    message: "Sorry, there are no images matching your search query. Please try again!",
+                    position: "topRight",
+                });
+            } else {
+                displayImages(data.hits);
+                
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching images:", error);
+        });
+};
 
 function handleSubmit(event) {
     event.preventDefault();
-
     const searchText = searchInput.value.trim();
     if (searchText === "") {
         iziToast.error({
@@ -29,28 +63,31 @@ function handleSubmit(event) {
         });
     } else {
         makeRequest(searchText);
-           btnSubmit.disabled = false; 
+         btnSubmit.disabled = false;
+       
     }
 }
+     
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = "";
+// const galleryListHtml = images 
+//         .map((image) => `
+//     <li class="gallery-item">
+// 	<a class="gallery-link" href="${image.original}">
+// 		<img 
+// 			class="gallery-image" 
+// 			src="${image.preview}" 
+// 			alt="${image.description}" 
+// 			/>
+// 	</a>
+// </li>`).join("");
 
-function makeRequest(searchText) {
-   const params = new URLSearchParams({
-    key: API_KEY,
-    q: Image,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: true
-}); 
-}
-
-fetch(`${BASE_URL} ? ${params}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(response.status)
-        }
-        return response.json()
+// galleryList.innerHTML = galleryListHtml;
+    const lightbox = new simpleLightbox(`.gallery a`, {
+        captionType: `attr`,
+        captionsData: `alt`,
+        captionDelay: 250,
     });
 
-  
   
 export { makeRequest };
